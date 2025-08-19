355a819adb2321023ed8fe32d916df3ab59e005a
mariusdicu <mdicu@jackrabbittech.com>
2025-08-19 09:48:58 -0500

    Initial commit

---
+++ src/test/java/com/rahulshetty/Methods.java
@@ -0,0 +1,22 @@
+package com.rahulshetty;
+
+public class Methods {
+
+    public void ValidateHeader(){
+
+        System.out.println("Heder links validated");
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/rahulshetty/Parent.java
@@ -0,0 +1,36 @@
+package com.rahulshetty;
+
+public class Parent {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+
+        int Sum = a+b;
+
+        System.out.println("Sum is: " + Sum);
+
+
+
+        System.out.println("I Navigate to home page");
+
+        Methods m = new Methods();
+
+        //Classobject.methodname();
+        m.ValidateHeader();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
